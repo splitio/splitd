95b594b2519a2a63b322db2a71d0c81c77b44ad1
Martin Redolatti <martinredolatti@gmail.com>
2023-01-23 12:36:57 -0300

    revamp config

--- cmd/splitd/main.go
+++ cmd/splitd/main.go
@@ -2,20 +2,19 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 
 	"github.com/splitio/go-toolkit/v5/logging"
+	"github.com/splitio/splitd/splitio/conf"
 	"github.com/splitio/splitd/splitio/link"
 	"github.com/splitio/splitd/splitio/sdk"
 	"github.com/splitio/splitd/splitio/util"
-	"gopkg.in/yaml.v3"
 )
 
 func main() {
 
-	cfg, err := readConfig()
+	cfg, err := conf.ReadConfig()
 	if err != nil {
 		fmt.Println("error reading config: ", err.Error())
 		os.Exit(1)
@@ -26,9 +25,9 @@ func main() {
 		LogLevel: logging.LevelInfo,
 	})
 
-	splitSDK, err := sdk.New(logger, cfg.Apikey)
+	splitSDK, err := sdk.New(logger, cfg.SDK.Apikey, cfg.SDK.ToSDKConf()...)
 
-	errc, lShutdown, err := link.Listen(logger, splitSDK, cfg.Link.toLinkOpts()...)
+	errc, lShutdown, err := link.Listen(logger, splitSDK, cfg.Link.ToLinkOpts()...)
 	if err != nil {
 		logger.Error("startup error: ", err)
 		os.Exit(1)
@@ -49,59 +48,3 @@ func main() {
 		logger.Error(err)
 	}
 }
-
-// -- Config
-
-const defaultConfigFN = "/etc/splitd.yaml"
-
-type config struct {
-	Apikey string `yaml:"apikey"`
-	Link   Link   `yaml:"link"`
-}
-
-func (c *config) parse(fn string) error {
-
-	raw, err := ioutil.ReadFile(fn)
-	if err != nil {
-		return fmt.Errorf("error reading yaml file: %w", err)
-	}
-
-	err = yaml.Unmarshal(raw, c)
-	if err != nil {
-		return fmt.Errorf("error parsing yaml file: %w", err)
-	}
-
-	return nil
-}
-
-type Link struct {
-	Type          *string `yaml:"type"`
-	Address       *string `yaml:"address"`
-	Serialization *string `yaml:"serialization"`
-}
-
-func (l Link) toLinkOpts() []link.Option {
-	var opts []link.Option
-	if l.Type != nil {
-		opts = append(opts, link.WithSockType(*l.Type))
-	}
-	if l.Address != nil {
-		opts = append(opts, link.WithAddress(*l.Address))
-	}
-	if l.Serialization != nil {
-		opts = append(opts, link.WithSerialization(*l.Serialization))
-	}
-	return opts
-}
-
-func readConfig() (*config, error) {
-	cfgFN := defaultConfigFN
-	if fromEnv := os.Getenv("SPLITD_CONF_FILE"); fromEnv != "" {
-		cfgFN = fromEnv
-	}
-
-	var c config
-	return &c, c.parse(cfgFN)
-}
-
-
--- splitd.yaml.tpl
+++ splitd.yaml.tpl
@@ -1,7 +1,17 @@
-apikey: "YOUR_API_KEY"
+# vim:ft=yaml
+sdk:
+  apikey: "YOUR_API_KEY"
+  urls:
+    auth: "https://auth.split.io",
+    sdk: "https://sdk.split.io/api",
+    events: "https://events.split.io/api",
+    streaming: "https://streaming.split.io/sse",
+    telemetry: "https://telemetry.split.io/api/v1",
+
 link:
-  type: "unix-stream"
+  type: "unix-seqpacket"
   address: "/var/run/splitd.sock"
   serialization: "msgpack"
+
 
 
