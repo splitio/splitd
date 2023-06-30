387873b56f452700a69e32e21fe5f62d502f70ff
Martin Redolatti <martin.redolatti@split.io>
2023-06-30 17:43:49 -0300

    add banner

---
+++ splitio/banner.go
@@ -0,0 +1,12 @@
+package splitio
+
+// For this we thank our dear friend & go mentor @sarrubia
+var ASCILogo = `
+     __      ____        _ _ _
+    / /__   / ___| _ __ | (_) |_
+   / / \ \  \___ \| '_ \| | | __|
+   \ \  \ \  ___) | |_) | | | |_
+    \_\ / / |____/| .__/|_|_|\__|
+       /_/        |_|
+
+       `
