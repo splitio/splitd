9a7664651454e68130cb74c31fc7b37ffaa473a4
Martin Redolatti <martin.redolatti@split.io>
2023-08-08 16:33:04 -0300

    more tests

--- splitio/link/protocol/v1/errors.go
+++ splitio/link/protocol/v1/errors.go
@@ -3,8 +3,6 @@ package v1
 import (
 	"errors"
 	"strconv"
-
-	"github.com/vmihailenco/msgpack/v5"
 )
 
 var (
@@ -12,32 +10,6 @@ var (
 	ErrOpcodeArgMismatch = errors.New("opcode doesn't match arguments type")
 	ErrIncorrectArgCount = errors.New("invalid argument count")
 )
-
-type InvocationErrorCode int
-
-const (
-	InvocationErrorInvalidArgs InvocationErrorCode = 0
-)
-
-type ErrorWithResponse interface {
-	ToResponse() []byte
-}
-
-type InvocationError struct {
-	code    InvocationErrorCode
-	message string
-}
-
-func (e *InvocationError) Error() string {
-	return e.message
-}
-
-func (e *InvocationError) ToResponse() []byte {
-	serialized, _ := msgpack.Marshal(e)
-	return serialized
-}
-
-// ---------------
 
 type RPCParseErrorCode int
 
@@ -56,7 +28,7 @@ func (c RPCParseErrorCode) formatWithData(data int64) string {
 	case PECInvalidArgType:
 		return "wrong argument type at index " + strconv.Itoa(int(data))
 	default:
-        return "unknown error"
+		return "unknown error"
 	}
 }
 
@@ -67,10 +39,7 @@ type RPCParseError struct {
 
 // Error implements error
 func (e RPCParseError) Error() string {
-    return e.Code.formatWithData(e.Data)
+	return e.Code.formatWithData(e.Data)
 }
 
 var _ error = RPCParseError{}
-
-// ----------------
-var _ ErrorWithResponse = (*InvocationError)(nil)
