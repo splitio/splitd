0f60317d480d944e951b94c303df2e0943b7b0ff
Martin Redolatti <martin.redolatti@split.io>
2023-08-08 18:17:59 -0300

    more coverage

---
+++ splitio/link/client/client_test.go
@@ -0,0 +1,39 @@
+package client
+
+import (
+	"testing"
+
+	"github.com/splitio/go-toolkit/v5/logging"
+	"github.com/splitio/splitd/splitio/link/protocol"
+	v1 "github.com/splitio/splitd/splitio/link/protocol/v1"
+	smocks "github.com/splitio/splitd/splitio/link/serializer/mocks"
+	"github.com/splitio/splitd/splitio/link/transfer/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestClientConstruction(t *testing.T) {
+	connMock := &mocks.RawConnMock{}
+	connMock.On("SendMessage", mock.Anything).Once().Return(nil)
+	connMock.On("ReceiveMessage").Once().Return([]byte("registerResp"), nil)
+    connMock.On("Shutdown").Once().Return(nil)
+	serializeMock := &smocks.SerializerMock{}
+	serializeMock.On("Serialize", mock.Anything).Once().Return([]byte("serializedRegister"), nil)
+	serializeMock.On("Parse", mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
+        args.Get(1).(*v1.ResponseWrapper[v1.RegisterPayload]).Status = v1.ResultOk
+    }).Return(nil)
+	logger := logging.NewLogger(nil)
+	c, err := New(logger, connMock, serializeMock, Options{Protocol: protocol.V1, ImpressionsFeedback: false})
+	assert.Nil(t, err)
+	assert.NotNil(t, c)
+	assert.Nil(t, c.Shutdown())
+}
+
+func TestClientUnknownProtocol(t *testing.T) {
+	connMock := &mocks.RawConnMock{}
+	serializeMock := &smocks.SerializerMock{}
+	logger := logging.NewLogger(nil)
+	c, err := New(logger, connMock, serializeMock, Options{Protocol: protocol.Version(252), ImpressionsFeedback: false})
+	assert.Nil(t, c)
+	assert.ErrorContains(t, err, "unknown protocol")
+}
---
+++ splitio/link/link_test.go
@@ -0,0 +1,60 @@
+package link
+
+import (
+	"testing"
+
+	"github.com/splitio/go-toolkit/v5/logging"
+	"github.com/splitio/splitd/splitio/link/client"
+	"github.com/splitio/splitd/splitio/link/protocol"
+	"github.com/splitio/splitd/splitio/link/serializer"
+	"github.com/splitio/splitd/splitio/link/transfer"
+	"github.com/splitio/splitd/splitio/sdk/mocks"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOptions(t *testing.T) {
+	assert.Equal(t, ListenerOptions{
+		Transfer:      transfer.DefaultOpts(),
+		Acceptor:      transfer.DefaultAcceptorConfig(),
+		Serialization: serializer.MsgPack,
+		Protocol:      protocol.V1,
+	},
+		DefaultListenerOptions())
+
+	assert.Equal(t, ConsumerOptions{
+		Transfer:      transfer.DefaultOpts(),
+		Consumer:      client.DefaultOptions(),
+		Serialization: serializer.MsgPack,
+	}, DefaultConsumerOptions())
+}
+
+func TestListenErrors(t *testing.T) {
+	lo := DefaultListenerOptions()
+	lo.Protocol = protocol.Version(123)
+	acc, shutdown, err := Listen(logging.NewLogger(nil), &mocks.SDKMock{}, &lo)
+	assert.Nil(t, acc)
+	assert.Nil(t, shutdown)
+	assert.ErrorContains(t, err, "protocol")
+
+	lo = DefaultListenerOptions()
+	lo.Serialization = serializer.Mechanism(123)
+	acc, shutdown, err = Listen(logging.NewLogger(nil), &mocks.SDKMock{}, &lo)
+	assert.Nil(t, acc)
+	assert.Nil(t, shutdown)
+	assert.ErrorContains(t, err, "serializer")
+}
+
+func TestConsumerErrors(t *testing.T) {
+	co := DefaultConsumerOptions()
+	co.Consumer.Protocol = protocol.Version(123)
+	client, err := Consumer(logging.NewLogger(nil), &co)
+	assert.Nil(t, client)
+	assert.ErrorContains(t, err, "protocol")
+
+	co = DefaultConsumerOptions()
+	co.Serialization = serializer.Mechanism(123)
+	client, err = Consumer(logging.NewLogger(nil), &co)
+	assert.Nil(t, client)
+	assert.ErrorContains(t, err, "serializer")
+
+}
