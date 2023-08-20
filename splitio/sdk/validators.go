0c50f5cfb1698893cafb7d31686d1fbc218e8768
Martin Redolatti <martin.redolatti@split.io>
2023-08-20 18:47:46 -0300

    add evenet properties validation

---
+++ splitio/sdk/validators.go
@@ -0,0 +1,49 @@
+package sdk
+
+import (
+	"errors"
+
+	"github.com/splitio/go-toolkit/v5/logging"
+)
+
+// MaxEventLength constant to limit the event size
+const MaxEventLength = 32768
+
+var ErrEventTooBig = errors.New("The maximum size allowed for the properties is 32kb. Event not queued")
+
+type Validator struct {
+	logger logging.LoggerInterface
+}
+
+func (i *Validator) validateTrackProperties(properties map[string]interface{}) (map[string]interface{}, int, error) {
+	if len(properties) == 0 {
+		return nil, 0, nil
+	}
+
+	if len(properties) > 300 {
+		i.logger.Warning("Track: Event has more than 300 properties. Some of them will be trimmed when processed")
+	}
+
+	processed := make(map[string]interface{})
+	size := 1024 // Average event size is ~750 bytes. Using 1kbyte as a starting point.
+	for name, value := range properties {
+		size += len(name)
+		switch value.(type) {
+		case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, float32, float64, bool, nil:
+			processed[name] = value
+		case string:
+			asStr := value.(string)
+			size += len(asStr)
+			processed[name] = value
+		default:
+			i.logger.Warning("Property %s is of invalid type. Setting value to nil")
+			processed[name] = nil
+		}
+
+		if size > MaxEventLength {
+			i.logger.Error("The maximum size allowed for the properties is 32kb. Event not queued")
+			return nil, size, ErrEventTooBig
+		}
+	}
+	return processed, size, nil
+}
