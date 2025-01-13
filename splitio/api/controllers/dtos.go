bbb060e17b23d8f697d668c08a40b9e4e0c0e2d1
Martin Redolatti <martin.redolatti@harness.io>
2025-01-13 17:07:49 -0300

    add checklFalg endpoint

---
+++ splitio/api/controllers/dtos.go
@@ -0,0 +1,12 @@
+package controllers
+
+type SplitViewDTO struct {
+	Name             string            `json:"name"`
+	TrafficType      string            `json:"trafficType"`
+	Killed           bool              `json:"killed"`
+	Treatments       []string          `json:"treatments"`
+	ChangeNumber     int64             `json:"changeNumber"`
+	Configs          map[string]string `json:"configs"`
+	DefaultTreatment string            `json:"defaultTreatment"`
+	Sets             []string          `json:"sets"`
+}
